68584133b628805dcaaf014027bc59f7ebd7154e
Quentin6710 <majdaquentin@gmail.com>
2025-04-23 05:38:40 +0200

    Ajout de 404.html et modification de index.html pour la gestion des routes SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 // src/App.tsx
 import React, { useState, useEffect } from "react";
-import { Routes, Route, useLocation, BrowserRouter } from "react-router-dom";
 import { Main, Project, Contact, Navigation, Footer } from "./components";
 import FadeIn from "./components/FadeIn";
 import PdfPage from "./components/PdfPage";
-import TwoFAMFA from "./components/2FAMFA";
-import Cryptomator from "./components/cryptomator";
+import TwoFAMFA from "./components/2FAMFA"; // Import de la page 2FAMFA
+import Cryptomator from "./components/cryptomator"; // Import de la page Cryptomator
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import "./index.scss";
 
-function MainContent() {
+function MainContent({ mode }: { mode: string }) {
   const location = useLocation();
   const isPdfPage = location.pathname === "/pdf";
 
   return (
     <>
-      {!isPdfPage ? (
+      {!isPdfPage && (
         <FadeIn transitionDuration={700}>
           <Main />
           <Project />
           <Contact />
         </FadeIn>
-      ) : (
+      )}
+      {isPdfPage && (
         <div style={{ marginTop: "80px" }}>
           <PdfPage />
         </div>
@@ -31,24 +32,27 @@ function MainContent() {
 
 function App() {
   const [mode, setMode] = useState<string>("dark");
-  const handleModeChange = () => setMode((prev) => (prev === "dark" ? "light" : "dark"));
+
+  const handleModeChange = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   }, []);
 
   return (
-    <BrowserRouter basename="/">
-      <div className={`main-container ${mode === "dark" ? "dark-mode" : "light-mode"}`}>
+    <div className={`main-container ${mode === "dark" ? "dark-mode" : "light-mode"}`}>
+      <Router>
         <Navigation parentToChild={{ mode }} modeChange={handleModeChange} />
         <Routes>
-          <Route path="/*" element={<MainContent />} />
-          <Route path="/2FAMFA" element={<TwoFAMFA />} />
-          <Route path="/cryptomator" element={<Cryptomator />} />
+          <Route path="/*" element={<MainContent mode={mode} />} />
+          <Route path="/2FAMFA" element={<TwoFAMFA />} /> {/* Route pour 2FAMFA */}
+          <Route path="/cryptomator" element={<Cryptomator />} /> {/* Route pour Cryptomator */}
         </Routes>
         <Footer />
-      </div>
-    </BrowserRouter>
+      </Router>
+    </div>
   );
 }
 
